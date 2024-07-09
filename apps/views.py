a811032a5255c86a592b1a1fe7764fbb9a80e7cc
mirmakhmudov16 <mr.mirmakhmudov16112008@gmail.com>
2024-07-10 03:52:45 +0500

    Commit

---
+++ apps/views.py
@@ -0,0 +1,13 @@
+import django.views.generic
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from ipware import get_client_ip
+import requests, json
+
+
+def index(request):
+    client_ip = get_client_ip(request)
+    response = requests.get(f"http://ip-api.com/json/{client_ip}")
+    l_data = response.text
+    ldata = json.loads(l_data)
+    return render(request, "index.html", context={"data": ldata})
